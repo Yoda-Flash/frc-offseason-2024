cf483b61a9704c964e21205de80bdd4bdcd382e6
deltoidal-icositetrahedron <86028471+deltoidal-icositetrahedron@users.noreply.github.com>
2024-01-11 19:03:23 -0800

    Made all the commands/subsystems for the elevator pivot

--- frc-2024/src/main/java/frc/robot/Constants.java
+++ frc-2024/src/main/java/frc/robot/Constants.java
@@ -13,7 +13,17 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
+  public static class ElevatorPivot {
     public static final int kDriverControllerPort = 0;
+    public static final int kElevatorPivotMotorID1 = 1;
+    public static final int kElevatorPivotMotorID2 = 2;
+    public static final int kElevatorPivotMotorID3 = 3;
+    public static final int kElevatorPivotMotorID4 = 4;
+    public static final int kPivotGearRatio = 200;
+    public static final double kNeoTicksPerRevolution = 42;
+    public static final double elevatorPivot_kP = 0.5;
+    public static final double elevatorPivot_kI = 0;
+    public static final double elevatorPivot_kD = 0;
+    public static final double kDeadBand = 0.05;
   }
 }
