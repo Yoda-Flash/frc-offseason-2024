c09d72784e2c0c8bcef9e2c951c4b8ab57a9e83b
deltoidal-icositetrahedron <86028471+deltoidal-icositetrahedron@users.noreply.github.com>
2024-01-18 17:02:59 -0800

    Created Elevator subsytem and command

---
+++ frc-2024/src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Elevator extends SubsystemBase {
+
+  private CANSparkMax m_neo1 = new CANSparkMax(Constants.Elevator.kMotorID1, MotorType.kBrushless);
+  private CANSparkMax m_neo2 = new CANSparkMax(Constants.Elevator.kMotorID2, MotorType.kBrushless);
+
+  /** Creates a new Elevator. */
+  public Elevator() {
+    m_neo2.setInverted(true);
+    m_neo1.getEncoder().setPosition(0);
+  }
+
+  public double getEncoderPosition(){
+    return m_neo1.getEncoder().getPosition();
+  }
+
+  public void setSpeed(double speed){
+    m_neo1.set(speed);
+    m_neo2.set(speed);
+  }
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
