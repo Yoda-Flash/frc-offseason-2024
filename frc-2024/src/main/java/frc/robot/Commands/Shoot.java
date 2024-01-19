ea273c8b75bbdcf93821c2f88d27acc80ece4bee
Wisteria840 <em.peng2020@gmail.com>
2024-01-18 20:36:28 -0800

    small update
    ignore my earlier mistakes pls

--- frc-2024/src/main/java/frc/robot/Commands/Shoot.java
+++ frc-2024/src/main/java/frc/robot/Commands/Shoot.java
@@ -54,6 +54,6 @@ public class Shoot extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return m_pid.atSetpoint();
+    return false;
   }
 }
