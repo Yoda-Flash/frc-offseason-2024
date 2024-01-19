d99625cb56cb75c2fd90f5c5c58ef1ab9c2a259f
deltoidal-icositetrahedron <86028471+deltoidal-icositetrahedron@users.noreply.github.com>
2024-01-18 17:02:59 -0800

    Created Elevator subsytem and command

---
+++ frc-2024/src/main/java/frc/robot/commands/MoveElevator.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Elevator;
+import frc.robot.Constants;
+
+public class MoveElevator extends Command {
+
+  private double m_endPosition;
+  private Elevator m_elevator;
+  private PIDController m_PID = new PIDController(Constants.Elevator.kP, Constants.Elevator.kI, Constants.Elevator.kD);
+  /** Creates a new MoveElevator. */
+  public MoveElevator(double d, Elevator e) {
+    m_elevator = e;
+    m_endPosition = d*Constants.Elevator.RotationsPerInch*Constants.kNeoTicksPerRevolution;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_elevator.setSpeed(m_PID.calculate(m_elevator.getEncoderPosition(), m_endPosition));
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_elevator.setSpeed(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return (Math.abs(m_elevator.getEncoderPosition() - m_endPosition) < Constants.ElevatorPivot.kDeadBand);
+  }
+}
