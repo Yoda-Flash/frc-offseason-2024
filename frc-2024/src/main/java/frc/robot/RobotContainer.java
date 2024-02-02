b7ab19eb890bb1de7a2c2a276b349bbd5541707a
Yoda-Flash <113494066+Yoda-Flash@users.noreply.github.com>
2024-02-01 20:33:45 -0800

    Transferred all swerve stuff to main repo

--- frc-2024/src/main/java/frc/robot/RobotContainer.java
+++ frc-2024/src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,17 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.commands.JoystickDrive;
+import frc.robot.subsystems.SwerveDrive;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -17,11 +24,25 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
+  private final SwerveDrive m_swerve = new SwerveDrive();
+
+  private final Joystick m_driverJoystick = new Joystick(DriveConstants.kDriveJoystickId);
+
+  private final JoystickDrive m_drive = new JoystickDrive(m_swerve, 
+    () -> -m_driverJoystick.getRawAxis(DriveConstants.kJoystickXAxis),
+    () -> -m_driverJoystick.getRawAxis(DriveConstants.kJoystickYxis),
+    () -> -m_driverJoystick.getRawAxis(DriveConstants.kJoystickRotAxis)
+  );
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
+  public RobotContainer() {    
+    m_swerve.setDefaultCommand(m_drive);
+
     // Configure the trigger bindings
+
     configureBindings();
+    
+    SmartDashboard.putData("Swerve/Reset_Heading", new InstantCommand(() -> m_swerve.resetHeading()));
   }
 
   /**
@@ -42,6 +63,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return null;
+    return new PathPlannerAuto("TestAuto");
   }
 }
---
+++ frc-2024/src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -0,0 +1,208 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+
+public class SwerveDrive extends SubsystemBase {
+  private final SwerveModule m_frontLeft = new SwerveModule(
+    DriveConstants.kFrontLeftDriveId, 
+    DriveConstants.kFrontLeftTurnId,
+    DriveConstants.kFrontLeftAbsoluteEncoderPort, 
+    DriveConstants.kFrontLeftAbsoluteEncoderOffset, 
+    DriveConstants.kFrontLeftDriveReversed,
+    DriveConstants.kFrontLeftTurningReversed, 
+    0
+  );
+
+  private final SwerveModule m_frontRight = new SwerveModule(
+    DriveConstants.kFrontRightDriveId, 
+    DriveConstants.kFrontRightTurnId,
+    DriveConstants.kFrontRightAbsoluteEncoderPort, 
+    DriveConstants.kFrontRightAbsoluteEncoderOffset, 
+    DriveConstants.kFrontRightDriveReversed, 
+    DriveConstants.kFrontRightTurningReversed, 
+    1
+  );
+
+  private final SwerveModule m_backLeft = new SwerveModule(
+    DriveConstants.kBackLeftDriveId, 
+    DriveConstants.kBackLeftTurnId,
+    DriveConstants.kBackLeftAbsoluteEncoderPort, 
+    DriveConstants.kBackLeftAbsoluteEncoderOffset, 
+    DriveConstants.kBackLeftDriveReversed, 
+    DriveConstants.kBackLeftTurningReversed, 
+    2
+  );
+
+  private final SwerveModule m_backRight = new SwerveModule(
+    DriveConstants.kBackRightDriveId, 
+    DriveConstants.kBackRightTurnId,
+    DriveConstants.kBackRightAbsoluteEncoderPort, 
+    DriveConstants.kBackRightAbsoluteEncoderOffset, 
+    DriveConstants.kBackRightDriveReversed, 
+    DriveConstants.kBackRightTurningReversed, 
+    3
+  );
+
+  private SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(m_frontLeft.getTranslation(), m_frontRight.getTranslation(), m_backLeft.getTranslation(), m_backRight.getTranslation());
+
+  private final AHRS m_imu = new AHRS();
+
+  private double m_totalCurrent;
+
+  private final PIDController m_xPID = new PIDController(DriveConstants.kP_X, DriveConstants.kI_X, DriveConstants.kD_X);
+  private final PIDController m_yPID = new PIDController(DriveConstants.kP_Y, DriveConstants.kI_Y, DriveConstants.kD_Y);
+  private final PIDController m_thetaPID = new PIDController(DriveConstants.kP_Theta, DriveConstants.kI_Theta, DriveConstants.kD_Theta);
+
+  private final SwerveDriveOdometry m_odo = new SwerveDriveOdometry(m_kinematics, getAngle(), new SwerveModulePosition[] {
+    m_frontLeft.getPosition(),
+    m_frontRight.getPosition(),
+    m_backLeft.getPosition(),
+    m_backRight.getPosition()
+  });
+
+  public void resetHeading() {
+    m_imu.reset();
+  }
+
+  public void resetOdo(Pose2d pose) {
+    m_odo.resetPosition(getAngle(), new SwerveModulePosition[] {
+      m_frontLeft.getPosition(),
+      m_frontRight.getPosition(),
+      m_backLeft.getPosition(),
+      m_backRight.getPosition()
+    }, pose);
+  }
+
+  public Pose2d getPoseMeters(){
+    return m_odo.getPoseMeters();
+  }
+
+  public SwerveDriveKinematics getSwerveKinematics(){
+    return m_kinematics;
+  }
+
+  public void setModuleStates(SwerveModuleState[] states) {
+    SwerveDriveKinematics.desaturateWheelSpeeds(states, DriveConstants.kMaxTranslationalMetersPerSecond);
+
+    m_frontLeft.setDesiredState(states[0]);
+    m_frontRight.setDesiredState(states[1]);
+    m_backLeft.setDesiredState(states[2]);
+    m_backRight.setDesiredState(states[3]);
+  }
+
+  public void setSwerveModuleStatesAuto(SwerveModuleState[] states) {
+    setModuleStates(states);  
+  }
+
+  public void printVelocitiesandPositions(){
+    System.out.println("Front left Velocity: " + m_frontLeft.getDriveVelocity());
+    System.out.println("Front left Position: " + m_frontLeft.getDrivePosition());
+    System.out.println("Front right velocity: " + m_frontRight.getDriveVelocity());
+    System.out.println("Front right Position: " + m_frontRight.getDrivePosition());
+    System.out.println("Back left velocity: " + m_backLeft.getDriveVelocity());
+    System.out.println("Back left Position: " + m_backLeft.getDrivePosition());
+    System.out.println("Back right velocity: " + m_backRight.getDriveVelocity());
+    System.out.println("Back right Position: " + m_backRight.getDrivePosition());
+  }
+
+  public Rotation2d getAngle() {
+    return Rotation2d.fromDegrees(Math.IEEEremainder(m_imu.getAngle(), 360));
+  }
+
+  public PIDController getXController(){
+    return m_xPID;
+  }
+
+  public PIDController getYController(){
+    return m_yPID;
+  }
+
+  public PIDController getThetaController(){
+    return m_thetaPID;
+  }
+  
+  public ChassisSpeeds getChassisSpeeds() { 
+    return DriveConstants.kDriveKinematics.toChassisSpeeds(
+      m_frontLeft.getState(), 
+      m_frontRight.getState(), 
+      m_backLeft.getState(), 
+      m_backRight.getState()
+    );
+  }
+
+  public void driveRobotRelative(ChassisSpeeds speeds) { 
+    SwerveModuleState[] states = DriveConstants.kDriveKinematics.toSwerveModuleStates(speeds);
+    setModuleStates(states);
+  }
+
+  public SwerveDrive(){
+     AutoBuilder.configureHolonomic(
+                this::getPoseMeters, // Robot pose supplier
+                this::resetOdo, // Method to reset odometry (will be called if your auto has a starting pose)
+                this::getChassisSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+                this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+                new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+                        new PIDConstants(0.5, 0.0, 0.0), // Translation PID constants
+                        new PIDConstants(0.5, 0.0, 0.0), // Rotation PID constants
+                        0.5, // Max module speed, in m/s
+                        0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+                        new ReplanningConfig() // Default path replanning config. See the API for the options here
+                ),
+                () -> {
+                    // Boolean supplier that controls when the path will be mirrored for the red alliance
+                    // This will flip the path being followed to the red side of the field.
+                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+                    var alliance = DriverStation.getAlliance();
+                    if (alliance.isPresent()) {
+                        return alliance.get() == DriverStation.Alliance.Red;
+                    }
+                    return false;
+                },
+                this // Reference to this subsystem to set requirements
+     );
+  }
+
+  public void stop() {
+    m_frontLeft.stop();
+    m_frontRight.stop();
+    m_backLeft.stop();
+    m_backRight.stop();
+  }
+
+  @Override
+  public void periodic() {
+    m_odo.update(getAngle(),
+      new SwerveModulePosition[] {
+        m_frontLeft.getPosition(), m_frontRight.getPosition(),
+        m_backLeft.getPosition(), m_backRight.getPosition()
+      });
+
+    SmartDashboard.putNumber("Angle", getAngle().getDegrees());
+    
+    m_totalCurrent = m_frontLeft.getDriveCurrent() + m_frontLeft.getTurnCurrent() + m_frontRight.getDriveCurrent() + m_frontRight.getTurnCurrent() + m_backLeft.getDriveCurrent() + m_backLeft.getTurnCurrent() + m_backRight.getDriveCurrent() + m_backRight.getTurnCurrent();
+    SmartDashboard.putNumber("Total Current", m_totalCurrent);
+  }
+}
