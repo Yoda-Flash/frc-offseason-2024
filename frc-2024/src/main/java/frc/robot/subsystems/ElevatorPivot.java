880fe9b1780a4657b97c9fae0bb80a75b471bdc8
celloii <133549470+celloii@users.noreply.github.com>
2024-02-17 10:46:02 -0800

    Update ElevatorPivot.java

--- frc-2024/src/main/java/frc/robot/subsystems/ElevatorPivot.java
+++ frc-2024/src/main/java/frc/robot/subsystems/ElevatorPivot.java
@@ -8,6 +8,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -42,6 +43,6 @@ public class ElevatorPivot extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    m_encoder.getDistance();
+    SmartDashboard.putNumber("Distance", m_encoder.getDistance());
   }
 }
