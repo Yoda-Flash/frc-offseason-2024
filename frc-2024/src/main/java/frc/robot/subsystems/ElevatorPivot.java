9eba82372ec68c0e71102eb07df14d27a9bf760e
celloii <133549470+celloii@users.noreply.github.com>
2024-02-17 10:52:04 -0800

    Added DutyCycleEncoder

--- frc-2024/src/main/java/frc/robot/subsystems/ElevatorPivot.java
+++ frc-2024/src/main/java/frc/robot/subsystems/ElevatorPivot.java
@@ -7,7 +7,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -18,7 +18,7 @@ public class ElevatorPivot extends SubsystemBase {
   private CANSparkMax m_neo2 = new CANSparkMax(Constants.ElevatorPivot.kMotorID2, MotorType.kBrushless);
   private CANSparkMax m_neo3 = new CANSparkMax(Constants.ElevatorPivot.kMotorID3, MotorType.kBrushless);
   private CANSparkMax m_neo4 = new CANSparkMax(Constants.ElevatorPivot.kMotorID4, MotorType.kBrushless);
-  private Encoder m_encoder = new Encoder(0,1);
+  private DutyCycleEncoder m_encoder = new DutyCycleEncoder(0);
 
 
   /** Creates a new ElevatorPivot. */
@@ -43,6 +43,6 @@ public class ElevatorPivot extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    SmartDashboard.putNumber("Distance", m_encoder.getDistance());
+    SmartDashboard.putNumber("Absolute Distance", m_encoder.getDistance());
   }
 }
