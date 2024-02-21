2bb9a5a4bd6d9b72dea195c4cc52103db4914195
Yoda-Flash <113494066+Yoda-Flash@users.noreply.github.com>
2024-02-20 16:57:58 -0800

    tested stuff at Burlingame with pathplanner

--- frc-2024/src/main/java/frc/robot/Constants.java
+++ frc-2024/src/main/java/frc/robot/Constants.java
@@ -66,7 +66,8 @@ public final class Constants {
     // public static double kTeleopMaxAccelMetersPerSecondSquared = 0.5;
     public static double kTeleopMaxAngularAccelRadiansPerSecondSquared = 4;
     // public static double kTeleopMaxAngularAccelRadiansPerSecondSquared = 0.5;
-    public static double kTeleopMaxSpeedMetersPerSecond = kMaxTranslationalMetersPerSecond/4.0;
+    // public static double kTeleopMaxSpeedMetersPerSecond = kMaxTranslationalMetersPerSecond/4.0;
+    public static double kTeleopMaxSpeedMetersPerSecond = kMaxTranslationalMetersPerSecond;
     // public static double kTeleopMaxTurningRadiansPerSecond = 4.0 * Math.PI;
     public static double kTeleopMaxTurningRadiansPerSecond = 1.5   * Math.PI;
     public static int kDriveJoystickId = 0;
