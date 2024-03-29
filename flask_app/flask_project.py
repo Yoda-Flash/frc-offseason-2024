4c5a53f99da645002464e094fcd8392d7c30503c
celloii <133549470+celloii@users.noreply.github.com>
2024-03-28 17:04:24 -0700

    updated flask_project

--- flask_app/flask_project.py
+++ flask_app/flask_project.py
@@ -1,4 +1,3 @@
-# an object of WSGI application  
 from __future__ import print_function
 from webcam import WebcamVideoStream
 from fps import FPS
@@ -55,12 +54,14 @@ def frames():
 
 def gen():
 	vid = cv2.VideoCapture(0)
+
 	options = apriltag.DetectorOptions(families="tag36h11")
 	detector = apriltag.Detector(options)
 	NetworkTables.initialize(server='10.8.40.2')
 	logging.basicConfig(level=logging.DEBUG)	
 	while True:
 		success, frame = vid.read()
+		# print(frame.shape)
 		if not success:
 			break
 		else:
@@ -79,6 +80,7 @@ def gen():
 				ptA = (int(ptA[0]), int(ptA[1]))
 				# draw the bounding box of the AprilTag detection
 				# cv2.line(frame, ptA, ptB, (0, 255, 0), 2)
+
 				# cv2.line(frame, ptB, ptC, (0, 255, 0), 2)
 				# cv2.line(frame, ptC, ptD, (0, 255, 0), 2)
 				# cv2.line(frame, ptD, ptA, (0, 255, 0), 2)
@@ -94,7 +96,9 @@ def gen():
 				
 				#distance from tag
 				#fov = math.atan(6.5/12)*(1/2) 
-				fov = 118 #fov for innomakerov9281
+				#fov = 118 
+				# fov = 2.05949 #radians
+				fov = 1.57
 				# apriltag_width=6.5 #inches
 				# focal_length= (0.25)/(2*math.tan(fov/2))
 				# per_width= math.sqrt((int(ptB[1])- int(ptA[1]))**2 + (int(ptB[0])- int(ptA[0]))**2)
@@ -106,20 +110,52 @@ def gen():
 				
 				# #angle from center
 				#angle = math.atan(cX-(624/2)/focal_length)*(1/2)
+
 				angle=fov*cX/640
 				angle=angle-(1/2)*fov
-				angle = str(angle)
-				cv2.putText(frame, "Angle:" + angle, (200,200),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+			
 				sd = NetworkTables.getTable("SmartDashboard")
 				sd.putNumber("angle", angle)
-				sd.getNumber('other',0)
+				a_value = sd.getNumber('robotTime',0)
+				
+				print("Angle: {}".format(angle))
+				print("Tag_ID: {}".format(r.tag_id)) 				
+				#v_fov = 0.785 
+				v_fov = 0.71
+				#v_fov = 1.256
+				v_angle = v_fov*cY/400
+				v_angle =(-1)*(v_angle - (1/2)*v_fov)
+				camera_angle =0.628
+				camera_height = 7.5
+				testing_height = 45
+				distance = 0
+				if r.tag_id==3 or r.tag_id==4 or r.tag_id==7 or r.tag_id==8 or r.tag_id==1 or r.tag_id==2 or r.tag_id==9 or r.tag_id==10:
+								distance = (51.875-camera_height)/(math.tan(v_angle+camera_angle))
+								#distance = (testing_height-camera_height)/math.tan(v_angle+camera_angle)
+					#distance = (testing_height-camera_height)/(math.tan(v_angle+camera_angle)*math.cos(angle))
+				elif r.tag_id==5 or r.tag_id==6:
+								distance = 59.97-camera_height/math.tan(v_angle+camera_angle)
+				elif r.tag_id==11 or r.tag_id==12 or r.tag_id==13 or r.tag_id==14 or r.tag_id==15 or r.tag_id==16:
+								distance = 47.63-camera_height/math.tan(v_angle+camera_angle)
+
+				# next steps: camera distortion-- check if angle is accurate
+				print("Distance: {}".format(distance))
+				distance = str(distance)
+				cv2.putText(frame, "Distance: "+ distance, (300,300),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+				angle = str(angle)
+				v_angle = v_angle * (180/math.pi) #back to degrees
+				cv2.putText(frame, "Angle:" + angle, (200,200),
+								cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+				v_angle = str(v_angle)
+				cv2.putText(frame, "Vertical Angle: "+ v_angle, (100,100),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+				
 			# show the output image after AprilTag detection
 			ret, buffer = cv2.imencode('.jpg', frame)
 			frame = buffer.tobytes()
 			yield (b'--frame\r\n'
 				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-	
 # A decorator used to tell the application 
 # which URL is associated function 
 @app.route('/')       
@@ -128,7 +164,7 @@ def index():
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame') 
+	return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame') 
 
 if __name__=='__main__': 
 	app.run(debug = True,host='0.0.0.0')
